289a1f7fe915c008942aa555bd502c4c85cea18a
lalitsharma17@hotmail.com <lalitsharma17@hotmail.com>
2021-09-05 12:21:15 +0800

    firstlesson

---
+++ madilibs/madilibs.py
@@ -0,0 +1,7 @@
+youtuber = "Lalit Sharma" #some random variable
+
+## a few ways to do this
+
+print("subscribe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subcribe to {youtuber}")
